086e2bee2e2e41df3f5bc740a8d9b39adf92be1b
Emily Rigby <emily.rigby@ovoenergy.com>
2018-04-06 09:50:37 +0100

    improve readability

--- docs/data/techList.py
+++ docs/data/techList.py
@@ -6,24 +6,27 @@ def removeChar(t, ch, li):
 
 file = open('survey results.csv')
 file.readline()
-tech = {'0': {}, '1': {}, '2': {}, '3': {}, '4': {}, '5': {}, '6': {}, '7': {}, '8': {}}
+tech = {'1': {}, '2': {}, '3': {}, '4': {}, '5': {}, '6': {}, '7': {}, '8': {}, '9': {}}
 
 #titles transforms the columns from the tech survey (in comments) to quadrants 
 #in the tech radar 0 - Hosting, 1 - Infrastructure, 2 - Framworks, 3 - Data Management
-titles = {'0': '0', #infrastructure
-  '1': '1', #CI/CD
-  '2': '2', #logging
-  '3': '2', #monitoring
-  '4': '1', #infrastructure provisioning
-  '5': '2', #caching
-  '6': '3', #datastore
-  '7': '1', #packaging
-  '8': '2' #load balancing
+titles = {'1': '0', #infrastructure
+  '2': '1', #CI/CD
+  '3': '2', #logging
+  '4': '2', #monitoring
+  '5': '1', #infrastructure provisioning
+  '6': '2', #caching
+  '7': '3', #datastore
+  '8': '1', #packaging
+  '9': '2' #load balancing
   }
 for line in file:
   line = line.strip().split(',')
   counter = 0
   for column in line:
+    if counter == 0:
+      counter += 1
+      continue
     items = column.split("|")
     for item in items:
       item = item.lstrip().rstrip().lower()
@@ -34,5 +37,5 @@ for line in file:
 file = open('output', 'w')
 for t in tech:
   for i in tech[t]:
-    if i is not "none":
+    if i != "none":
       file.write(" {quadrant: "+titles[t]+",ring: 0,label: \""+i+"\",active: false,moved: 0},\n")
